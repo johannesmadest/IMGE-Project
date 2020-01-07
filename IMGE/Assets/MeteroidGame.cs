f78dbb2e1aa53aecd5d0ad2b0f08f02d59f3643a
Johannes Madest <johannesmadest@gmail.com>
2020-01-07 14:16:09 +0100

    meteroid

---
+++ IMGE/Assets/MeteroidGame.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteroidGame : MonoBehaviour
+{
+
+    public GameObject meteroid;
+
+    public bool finished = false;
+
+    private int currentTime;
+
+    public int startTime;
+
+    public float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime <= 0)
+        {
+            finished = true;
+            StartCoroutine(End());
+        }
+    }
+
+    IEnumerator Spawn()
+    {
+        while (!finished)
+        {
+            float speed = 2;
+            float angle = 42;
+            Vector3 start = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
+            Vector3 direction = Vector3.down;
+            GameObject peter = Instantiate(meteroid);
+            Meteroid m = peter.GetComponent<Meteroid>();
+            m.Config(speed, start, direction);
+        }
+
+        yield return 0;
+    }
+
+    IEnumerator End()
+    {
+        yield return 0;
+    }
+}
---
+++ IMGE/Assets/Scripts/Meteroid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteroid : MonoBehaviour
+{
+    private Vector3 direction;
+
+    private Vector3 startingPoint;
+
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = startingPoint;
+        direction.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    public void Config(float speed, Vector3 start, Vector3 direction)
+    {
+        this.speed = speed;
+        this.startingPoint = start;
+        this.direction = direction;
+    }
+}
